88e4f9e4a11d086ee97dd72600923ec772949e85
Fábio André Zatta <fisiofaz8@gmail.com>
2026-01-03 13:54:15 -0300

    feat: configura PWA e instala botao de whatsapp

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#2563eb", // Azul (cor principal do seu sistema)
     icons: [
       {
-        src: "/public/android-chrome-192x192.png",
+        src: "./public/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/public/android-chrome-512x512.png",
+        src: "./public/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
